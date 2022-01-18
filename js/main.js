7a0debdaa0537c7bb46ae715cf6969b0a69db6e7
Jiuhe1129 <66043476+Jiuhe1129@users.noreply.github.com>
2022-01-18 15:07:59 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,28 +4,25 @@ var scrollFunc = function (e) {
   if (e.wheelDelta) { //判断浏览器IE，谷歌滑轮事件             
     if (e.wheelDelta < 0) { //当滑轮向下滚动时
       $(".box4").css("top", "0")
-      $(".box4").css("opacity","1")
+      $(".box4").css("opacity", "1")
       $(".box1>div").css("opacity", "0")
-      
-      $(".box1").css("top","-100%")
-      $(".box1").css("left","0")
-      $(".box1>div").css("opacity","0")
-      $(".box3").css("left","100%")
+
+      $(".box1").css("top", "-100%")
+      $(".box1").css("left", "0")
+      $(".box1>div").css("opacity", "0")
+      $(".box3").css("left", "100%")
     }
     else {
-      $(".box1").css("top","0%")
+      $(".box1").css("top", "0%")
       $(".box4").css("top", "100%")
-      $(".box4").css("opacity","0")
+      $(".box4").css("opacity", "0")
       $(".box1>div").css("opacity", "1")
     }
     if (e.wheelDelta > 0) { //当滑轮向上滚动时
     }
   }
 }
-// //给页面绑定滑轮滚动事件
-// if (document.addEventListener) {
-//   document.addEventListener('DOMMouseScroll', scrollFunc, false);
-// }
+
 //滚动滑轮触发scrollFunc方法
 window.onmousewheel = document.onmousewheel = scrollFunc;
 
@@ -43,11 +40,11 @@ $(".n").mousemove(function () {
 
 
 $(".hire img,.lxw").mousemove(function () {
-    $(".hire").css("transform", "scale(1.1)")
-  }); $(".hire img,.lxw").mouseout(function () {
-    $(".hire").css("transform", "scale(1)")
-  });
-  
+  $(".hire").css("transform", "scale(1.1)")
+}); $(".hire img,.lxw").mouseout(function () {
+  $(".hire").css("transform", "scale(1)")
+});
+
 
 $(".fxk").mousemove(function () {
   $(".fxk").css("transform", "scale(1.1)")
@@ -80,26 +77,25 @@ $(".b").mousemove(function () {
 });
 
 
-
 $(".hire").click(function () {
-  $(".box1>div").css("opacity","0")
-  $(".box3").css("left","0")
-  $(".box3>div").css("opacity","1")
-setTimeout(function () {
-  $(".box3").css("background","url(img/psp.jpg) center no-repeat")
-  $(".left").css("bottom","-10%")
-  $(".right").css("top","0")
-}, 200)
+  $(".box1>div").css("opacity", "0")
+  $(".box3").css("left", "0")
+  $(".box3>div").css("opacity", "1")
+  setTimeout(function () {
+    $(".box3").css("background", "url(img/psp.jpg) center no-repeat")
+    $(".left").css("bottom", "-10%")
+    $(".right").css("top", "0")
+  }, 200)
 })
 $(".a").click(function () {
-  $(".box1>div").css("opacity","1")
-  $(".box3").css("left","100%")
-  $(".box3>div").css("opacity","0")
-setTimeout(function () {
-  $(".box3").css("background","url(img/banner-a.jpg) center no-repeat")
-  $(".left").css("bottom","-60%")
-  $(".right").css("top","-60%")
-}, 200)
+  $(".box1>div").css("opacity", "1")
+  $(".box3").css("left", "100%")
+  $(".box3>div").css("opacity", "0")
+  setTimeout(function () {
+    $(".box3").css("background", "url(img/banner-a.jpg) center no-repeat")
+    $(".left").css("bottom", "-60%")
+    $(".right").css("top", "-60%")
+  }, 200)
 })
 
 
@@ -113,7 +109,10 @@ setTimeout(function () {
 
 
 
-
-
-
-
+$(function(){
+  //获取浏览器宽度
+  var _width = $(window).width(); 
+  if(_width < 1500){
+         $(".i").remove();
+  }
+});

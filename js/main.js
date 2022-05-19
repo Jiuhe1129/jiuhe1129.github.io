b357f52b9db6854d3b489c9e3790b09e75a1cf95
旧鹤 <66043476+Jiuhe1129@users.noreply.github.com>
2022-05-19 11:40:03 +0800

    优化样式

--- js/main.js
+++ js/main.js
@@ -157,33 +157,30 @@ $(function () {
   }
 });
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
   var addEventListener = 'addEventListener';
   var elems = document.getElementsByClassName('box4');
   for (var i = 0; i < elems.length;) {
-    (function (elem, lastClientX, lastClientY, pushed) {
-      elem[addEventListener]('mousedown', function (e) {
-        pushed = 1;
-        lastClientX = e.clientX;
-        lastClientY = e.clientY;
+      (function(elem, lastClientX,  pushed) {
+          elem[addEventListener]('mousedown', function(e) {
+              pushed = 1;
+              lastClientX = e.clientX;
 
-        e.preventDefault();
-        e.stopPropagation();
-      }, 0);
+              e.preventDefault();
+              e.stopPropagation();
+          }, 0);
+          
+          window[addEventListener]('mousemove', function(e) {
+              if (pushed) {
+                  elem.scrollLeft -=
+                      (- lastClientX + (lastClientX=e.clientX));
+              }
+          }, 0);
+           
+          window[addEventListener]('mouseup', function(){
+              pushed = 0;
+          }, 0);
 
-      window[addEventListener]('mousemove', function (e) {
-        if (pushed) {
-          elem.scrollLeft -=
-            (- lastClientX + (lastClientX = e.clientX));
-          elem.scrollTop -=
-            (- lastClientY + (lastClientY = e.clientY));
-        }
-      }, 0);
-
-      window[addEventListener]('mouseup', function () {
-        pushed = 0;
-      }, 0);
-
-    })(elems[i++]);
+       })(elems[i++]);
   }
 }, 0);

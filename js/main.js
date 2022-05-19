723199e5f02cc5abe7503542f2b22ae1235427a8
旧鹤 <66043476+Jiuhe1129@users.noreply.github.com>
2022-05-19 11:15:39 +0800

    完善了一点点东西

--- js/main.js
+++ js/main.js
@@ -156,3 +156,34 @@ $(function () {
     $(".l").remove();
   }
 });
+
+window.addEventListener("load", function() {
+  var addEventListener = 'addEventListener';
+  var elems = document.getElementsByClassName('box4');
+  for (var i = 0; i < elems.length;) {
+      (function(elem, lastClientX, lastClientY, pushed) {
+          elem[addEventListener]('mousedown', function(e) {
+              pushed = 1;
+              lastClientX = e.clientX;
+              lastClientY = e.clientY;
+
+              e.preventDefault();
+              e.stopPropagation();
+          }, 0);
+          
+          window[addEventListener]('mousemove', function(e) {
+              if (pushed) {
+                  elem.scrollLeft -=
+                      (- lastClientX + (lastClientX=e.clientX));
+                  elem.scrollTop -=
+                      (- lastClientY + (lastClientY=e.clientY));
+              }
+          }, 0);
+           
+          window[addEventListener]('mouseup', function(){
+              pushed = 0;
+          }, 0);
+
+       })(elems[i++]);
+  }
+}, 0);
